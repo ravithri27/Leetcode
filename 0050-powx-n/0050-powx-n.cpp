fd687f7a7efc1d19662f37c284adc984ac38309a
ravithri27 <ravithri27@gmail.com>
2026-09-12 09:42:08 +0530

    Time: 0 ms (100%), Space: 8.7 MB (44.53%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long N = n;
+
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+
+        return fastPower(x, N);
+    }
+
+    static double fastPower(double x, long n) {
+        if (n == 0)
+            return 1;
+
+        double half = fastPower(x, n / 2);
+
+        if (n % 2 == 0)
+            return half * half;
+        else
+            return x * half * half;
+    }
+};

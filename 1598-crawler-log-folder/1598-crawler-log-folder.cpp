53750b9fd72c27d7e892a92a5e68d8aee90fc5ea
ravithri27 <ravithri27@gmail.com>
2026-09-14 20:32:35 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1598-crawler-log-folder/1598-crawler-log-folder.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        stack<string> st;
+        for(auto x:logs){
+            if(x=="../"&&!st.empty()){
+                st.pop();
+            }else if(x=="./"){
+                continue;
+            }else{
+                st.push(x);
+            }
+        }
+        return st.size();
+    }
+};

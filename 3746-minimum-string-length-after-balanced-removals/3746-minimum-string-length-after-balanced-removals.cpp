2e90bba41a35b4885824cad49c9da8a49bfa4c50
ravithri27 <ravithri27@gmail.com>
2026-09-16 19:25:03 +0530

    Time: 6 ms (37.04%), Space: 16.3 MB (9.93%) - LeetHub

---
+++ 3746-minimum-string-length-after-balanced-removals/3746-minimum-string-length-after-balanced-removals.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minLengthAfterRemovals(string s) {
+        stack<char> st;
+        for(auto x:s){
+            if(!st.empty()&&st.top()!=x){
+                st.pop();
+            }else{
+                st.push(x);
+            }
+        }
+        return st.size();
+    }
+};
